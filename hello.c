32294003fc9793cae76a9c97ef16acfe1a88c453
IloveRoblox4 <IloveRoblox4@users.noreply.github.com>
2019-01-17 10:22:10 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Evan Cornwell
+ * Date: 1/17/2019
+ *
+ * A simple Hello program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+ 
+  printf("Hello, my name is Evan, I don't have a major"\n);
+  
+
+  return 0;
+}
